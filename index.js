de15f7592d7176768722f8cb794987cdf2fbac44
Yvonne-del <ys359375@gmail.com>
2025-03-07 12:24:34 +0300

    complex tasks

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() *10)+1;
+const num3= 18;
+const num4= 7;
+const mod = num3 % num4;
+const num5 = 15;
+const num6 = 20;
+const max = Math.max(num5, num6)
